70fd9cd18196e244f6d6e3ce9d511faaf4cfbc28
Adil TAGUIA <atagu@lmdsio.com>
2026-09-03 14:11:20 +0200

    Mon projet version 2

--- accueil.php
+++ accueil.php
@@ -2,5 +2,19 @@
 $prenom = $_POST['prenom'];
 $nom = $_POST['nom'];
 echo "Bonjour  ",$nom;
+
+
+$login = $_POST['...'];
+$mdp = $_POST['...'];
+$host = $_SERVER['HTTP_HOST']; // on récupère le nom de l'hôte
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+
+if ($login == 'admin' AND $mdp == 'azerty' ) {
+ header("Location: http://$host$uri/profil.html"); // vers profil.html
+} else {
+ header("Location: http://$host$uri/index.html); // vers index.html
+}
+
+
 ?>
 
